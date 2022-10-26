68455e80cd9d6e9af028587545799acd751378ca
Lennard Kittner <lennardkittner@icloud.com>
2022-10-26 12:09:05 +0200

    initial commit

---
+++ Caffeinate/CaffeinateApp.swift
@@ -0,0 +1,15 @@
+//
+//  CaffeinateApp.swift
+//  Caffeinate
+//
+//  Created by Lennard on 26.10.22.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinateApp: App {
+    var body: some Scene {
+       
+    }
+}
