7479cd722cab17b658b6ef51105c25e763d59728
Lennard Kittner <lennardkittner@icloud.com>
2022-10-26 13:52:32 +0200

    Added sleep prevention.

--- Caffeinate/Appdelegate.swift
+++ Caffeinate/Appdelegate.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 import AppKit
+import IOKit.pwr_mgt
+
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBarItem: NSStatusItem?
     var hasCoffee = false
-    
+    var hardMode = false
+    var caffeinateTask :Progress?
+    var noSleepAssertionID: IOPMAssertionID = 0
+    var noSleepReturn: IOReturn?
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         updateState(coffee: hasCoffee)
@@ -25,15 +31,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func updateState(coffee: Bool) {
         if coffee {
-            statusBarItem?.button?.image = NSImage(
-                systemSymbolName: "cup.and.saucer",
-                accessibilityDescription: nil
-              )
-        } else {
+            preventSleep(hardMode)
             statusBarItem?.button?.image = NSImage(
                 systemSymbolName: "cup.and.saucer.fill",
                 accessibilityDescription: nil
               )
+        } else {
+            reEnableSleep(hardMode)
+            statusBarItem?.button?.image = NSImage(
+                systemSymbolName: "cup.and.saucer",
+                accessibilityDescription: nil
+              )
         }
+    }
+        
+    func preventSleep(_ hardMode: Bool) {
+        if !hardMode {
+            disableScreenSleep(reason: "Caffeinate")
+        }
+    }
+    
+    func reEnableSleep(_ hardMode: Bool) {
+        if !hardMode {
+            enableScreenSleep()
+        }
+    }
+  
+    // https://stackoverflow.com/questions/37601453/using-swift-to-disable-sleep-screen-saver-for-osx
+    
+    func disableScreenSleep(reason: String = "Unknown reason") -> Bool? {
+        guard noSleepReturn == nil else { return nil }
+        noSleepReturn = IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString,
+                                                IOPMAssertionLevel(kIOPMAssertionLevelOn),
+                                                reason as CFString,
+                                                &noSleepAssertionID)
+        return noSleepReturn == kIOReturnSuccess
+    }
+
+    func enableScreenSleep() -> Bool {
+        if noSleepReturn != nil {
+            _ = IOPMAssertionRelease(noSleepAssertionID) == kIOReturnSuccess
+            noSleepReturn = nil
+            return true
+        }
+        return false
     }
 }
