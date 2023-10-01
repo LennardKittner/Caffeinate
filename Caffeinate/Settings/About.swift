a0fb0b7ffdb8f7321d206378276493adb612d64c
Lennard Kittner <lennardkittner@icloud.com>
2023-10-01 23:09:13 +0200

    fixed version

--- Caffeinate/Settings/About.swift
+++ Caffeinate/Settings/About.swift
@@ -18,7 +18,7 @@ struct About: View {
                 .padding(.bottom, 10)
             Spacer()
                 .frame(height: 10)
-            Text("Version: \(version ?? "1.0.1")")
+            Text("Version: \(version ?? "1.0.2")")
                 .font(.subheadline)
             Text("Author: Lennard Kittner")
                 .font(.subheadline)
